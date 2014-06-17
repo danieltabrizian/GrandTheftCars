fa6a24fdcc2022657df211e080162256d0bb0f09
TWSSYesterday <thetryhardnoob@gmail.com>
2014-06-17 11:44:32 +0100

    I'm Cinderella
    Cleaning...

--- src/me/lawhit/gtc/ChatType.java
+++ src/me/lawhit/gtc/ChatType.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 public enum ChatType {
 	good,error,normal,important;
 
-
 public String getPrefix(){
 	return ChatColor.DARK_AQUA + "[" + ChatColor.RED+  "GTC" + ChatColor.DARK_AQUA + "] ";
 	
